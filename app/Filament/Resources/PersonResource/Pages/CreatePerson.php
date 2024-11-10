26d96be5b0ddd496a585a288049b4a5199aeaaad
Hamza-Mughales <hamzawemughales@gmail.com>
2024-11-10 20:06:32 +0300

    People update

--- app/Filament/Resources/PersonResource/Pages/CreatePerson.php
+++ app/Filament/Resources/PersonResource/Pages/CreatePerson.php
@@ -8,4 +8,18 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePerson extends CreateRecord
 {
     protected static string $resource = PersonResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (!empty($data['social_links'])) {
+            $data['social_links'] = array_map('trim', explode(',', $data['social_links']));
+        }
+    
+        return $data;
+    }
 }
