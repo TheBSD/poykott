caf192da6ec198fc2c4982a6874915a3d3d31986
Hamza-Mughales <hamzawemughales@gmail.com>
2024-11-10 17:16:36 +0300

    Funding Levels updates

--- app/Filament/Resources/FundingLevelResource/Pages/EditFundingLevel.php
+++ app/Filament/Resources/FundingLevelResource/Pages/EditFundingLevel.php
@@ -10,10 +10,8 @@ class EditFundingLevel extends EditRecord
 {
     protected static string $resource = FundingLevelResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getRedirectUrl(): string
     {
-        return [
-            Actions\DeleteAction::make(),
-        ];
+        return $this->getResource()::getUrl('index');
     }
 }
