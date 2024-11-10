eb5e4c54674d482e3f692c8c675414da3633c1a8
Hamza-Mughales <hamzawemughales@gmail.com>
2024-11-10 20:35:16 +0300

    Tags updates

--- app/Filament/Resources/TagResource/Pages/CreateTag.php
+++ app/Filament/Resources/TagResource/Pages/CreateTag.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateTag extends CreateRecord
 {
     protected static string $resource = TagResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/TagResource/Pages/EditTag.php
+++ app/Filament/Resources/TagResource/Pages/EditTag.php
@@ -10,10 +10,8 @@ class EditTag extends EditRecord
 {
     protected static string $resource = TagResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getRedirectUrl(): string
     {
-        return [
-            Actions\DeleteAction::make(),
-        ];
+        return $this->getResource()::getUrl('index');
     }
 }
