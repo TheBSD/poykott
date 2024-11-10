242890f08b49e2329b589460d302e1fbe2fd89a5
Hamza-Mughales <hamzawemughales@gmail.com>
2024-11-10 17:00:17 +0300

    Company Resources updates

--- app/Filament/Resources/CompanyResourceResource.php
+++ app/Filament/Resources/CompanyResourceResource.php
@@ -4,22 +4,29 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\CompanyResourceResource\Pages;
 use App\Models\CompanyResources;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class CompanyResourceResource extends Resource
 {
     protected static ?string $model = CompanyResources::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-cursor-arrow-ripple';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('title')->required(),
+                TextInput::make('url')->required(),
+                Select::make('company_id')
+                    ->searchable()->preload()
+                    ->relationship('company', 'name'),
             ]);
     }
 
@@ -27,13 +34,19 @@ class CompanyResourceResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('company.name')->numeric()->sortable()->searchable(),
+                TextColumn::make('title')->searchable()->sortable(), 
+                TextColumn::make('url')
+                ->url(fn(CompanyResources $record) => $record->url)
+                ->color('info')
+                ->openUrlInNewTab()->searchable()->limit(50), 
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Filament/Resources/CompanyResourceResource/Pages/CreateCompanyResource.php
+++ app/Filament/Resources/CompanyResourceResource/Pages/CreateCompanyResource.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCompanyResource extends CreateRecord
 {
     protected static string $resource = CompanyResourceResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
