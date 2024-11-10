44036b2775601ef94299eb4f3708605fde288983
Hamza-Mughales <hamzawemughales@gmail.com>
2024-11-10 20:23:21 +0300

    Person Resources updates

--- app/Filament/Resources/PersonResourcesResource/Pages/EditPersonResources.php
+++ app/Filament/Resources/PersonResourcesResource/Pages/EditPersonResources.php
@@ -10,10 +10,8 @@ class EditPersonResources extends EditRecord
 {
     protected static string $resource = PersonResourcesResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getRedirectUrl(): string
     {
-        return [
-            Actions\DeleteAction::make(),
-        ];
+        return $this->getResource()::getUrl('index');
     }
 }
