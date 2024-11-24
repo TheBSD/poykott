b4e9e462550921ded7955d800ec5c2eb84295c13
TheBSD <watheq725@gmail.com>
2024-11-25 00:28:59 +0300

    improve code quality tools and download people image command

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    use HasFactory;
-
     protected $fillable = ['path', 'type'];
 
     // image types enum
