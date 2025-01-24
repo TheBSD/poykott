d1d7c3eba1b3eadfab49f6ee397e5bfb781b628f
Eslam-Nazer <eslam.nazer43@gmail.com>
2025-01-24 23:45:01 +0200

    Company new columns

---
+++ database/migrations/2025_01_24_201134_new_columns_to_companies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('companies', function (Blueprint $table): void {
+            $table->string('email')->after('slug');
+            $table->string('personal_email')
+                ->after('email')
+                ->nullable();
+            $table->string('icon_url')
+                ->after('url')
+                ->nullable();
+            $table->string('tags')
+                ->after('notes')
+                ->nullable();
+            $table->string('office_locations')
+                ->after('tags')
+                ->nullable();
+            $table->string('resources')
+                ->after('office_locations')
+                ->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('companies', function (Blueprint $table): void {
+            $table->dropColumn([
+                'email',
+                'icon_url',
+                'tags',
+                'office_locations',
+                'resources',
+            ]);
+        });
+    }
+};
