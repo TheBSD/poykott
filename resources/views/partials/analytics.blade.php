87bca8c6c9d040d519fcec693f153e9e7ccca075
TheBSD <watheq725@gmail.com>
2025-01-25 01:20:58 +0300

    feat: better checks for Google Analytics

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -1,5 +1,11 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google.analytics_id') }}"></script>
+@if (config('services.google.analytics_id'))
+    <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google.analytics_id') }}"
+    ></script>
+@endif
+
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag() {
@@ -7,6 +13,8 @@
     }
     gtag('js', new Date());
 
-    gtag('config', '{{ config('services.google.analytics_id') }}');
+    gtag('config', '{{ config('services.google.analytics_id') }}', {
+        anonymize_ip: true, // todo look at this option. if not useful, then remove
+    });
 </script>
 <!-- End Google Analytics -->
