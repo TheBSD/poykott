d35b5626f3b41bc08a020f48e26d4acd08c440bc
TheBSD <watheq725@gmail.com>
2025-01-31 14:44:02 +0300

    chore: comment toBeOne function

--- tests/Pest.php
+++ tests/Pest.php
@@ -33,9 +33,9 @@ pest()
 |
 */
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
+//expect()->extend('toBeOne', function () {
+//    return $this->toBe(1);
+//});
 
 /*
 |--------------------------------------------------------------------------
