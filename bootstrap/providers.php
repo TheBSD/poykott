33fb94e96c22041ebc638ae6df5b043d6a245d0a
TheBSD <watheq725@gmail.com>
2025-02-20 22:51:15 +0300

    remove horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,10 +2,8 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
-use App\Providers\HorizonServiceProvider;
 
 return [
     AppServiceProvider::class,
     AdminPanelProvider::class,
-    HorizonServiceProvider::class,
 ];
