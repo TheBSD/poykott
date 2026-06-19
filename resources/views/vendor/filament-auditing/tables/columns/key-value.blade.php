94eb6d29eaa341d138338fef957b94af624cca36
TheBSD <148907974+TheBSD@users.noreply.github.com>
2026-06-19 20:48:19 +0000

    Dusting

--- resources/views/vendor/filament-auditing/tables/columns/key-value.blade.php
+++ resources/views/vendor/filament-auditing/tables/columns/key-value.blade.php
@@ -4,18 +4,18 @@
 
 <div class="my-2 text-sm font-medium tracking-tight">
     <ul>
-        @foreach($data ?? [] as $key => $value)
+        @foreach ($data ?? [] as $key => $value)
             <li>
                 <span class="inline-block rounded-md whitespace-normal text-gray-700 dark:text-gray-200 bg-gray-500/10">
                     {{ Str::title($key) }}:
                 </span>
                 <span class="font-semibold">
-                    @unless(is_array($value))
+                    @unless (is_array($value))
                        {{ str($value)->limit(30) }}
                     @else
                         <span class="divide-x divide-solid divide-gray-200 dark:divide-gray-700">
                             @foreach ($value as $nestedValue)
-                                {{$nestedValue['id']}}
+                                {{ $nestedValue['id'] }}
                             @endforeach
                         </span>
                     @endunless
